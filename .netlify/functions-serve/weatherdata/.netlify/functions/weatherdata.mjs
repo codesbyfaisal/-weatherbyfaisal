53884593a5b42237d40488c7be299fd81f37f642
codebyfaisal <fm0320939695@gmail.com>
2024-09-06 14:47:04 +0500

    adding dummy api key in .netlify/functions-serve/weatherdata/.netlify/functions/weatherdata.js

--- .netlify/functions-serve/weatherdata/.netlify/functions/weatherdata.mjs
+++ .netlify/functions-serve/weatherdata/.netlify/functions/weatherdata.mjs
@@ -6,12 +6,14 @@ let __filename=___nfyFileURLToPath(import.meta.url);
 let __dirname=___nfyPathDirname(___nfyFileURLToPath(import.meta.url));
 let require=___nfyCreateRequire(import.meta.url);
 
+adding dummy api key in .netlify/functions-serve/weatherdata/.netlify/functions/weatherdata.js
+
 
 // .netlify/functions/weatherdata.js
 var weatherdata_default = async (req, context) => {
   const url = new URL(req.url);
   const city = url.searchParams.get("city") || "peshawar";
-  const API_KEY = "0e6f4673b9f8af75f397f23f0e23bf4c";
+  const API_KEY = 'your api key of weather api';
   const api_url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
   const response = await fetch(api_url);
   const data = await response.json();
